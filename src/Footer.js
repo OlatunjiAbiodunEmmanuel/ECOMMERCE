c813ee957ac16efcd4e27bfee3a10b64145e1349
that9jaboy3 <olatunjia889@gmail.com>
2023-11-05 17:00:23 +0100

    feat:change footer

--- src/Footer.js
+++ src/Footer.js
@@ -2,7 +2,81 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='container-fluid bg-dark mt-5 text-light'>
+      <section className='d-md-flex justify-content-around aling-items-center pt-5'>
+      <div className='text-center mb-3'>
+        <h5>Where to buy</h5>
+        <span>See Authorized Retailers</span>
+      </div>
+
+      <div className='text-center mb-3'>
+        <h5>Sorry Rewards</h5>
+        <span>Join Now</span> <br></br>
+        <span>Learn More</span> <br></br>
+        <span>Manage Account</span>
+      </div>
+
+      <div className='text-center mb-3'>
+        <h5>News and Info</h5>
+        <span>Join Now</span> <br></br>
+        <span>Learn More</span> <br></br>
+        <span>Join Now</span> <br></br>
+        <span>Learn More</span> <br></br>
+        <span>Join Now</span> <br></br>
+        <span>Learn More</span> <br></br>
+        <span>Learn More</span> <br></br>
+        <span>Manage Account</span>
+      </div>
+
+      <div className='text-center mb-3'>
+        <h5>Other Sites</h5>
+        <span>Join Now</span> <br></br>
+        <span>Learn More</span> <br></br>
+        <span>Join Now</span> <br></br>
+        <span>Learn More</span> <br></br>
+        <span>Join Now</span> <br></br>
+        <span>Learn More</span> <br></br>
+        <span>Learn More</span> <br></br>
+        <span>Manage Account</span>
+      </div>
+      </section>
+
+      <section className='text-center mb-3 d-md-flex justify-content-around aling-items-center'>
+        <div>
+        <select>
+          <option>Nigeria</option>
+          <option>United State</option>
+          <option>United State</option>
+          <option>United State</option>
+          <option>United State</option>
+          <option>United State</option>
+        </select>
+        </div>
+
+        <div className='text-center mb-3 mt-4'>
+          <span>For Professionals</span>
+          <span>Carrers</span>
+          <span>Contact Us</span>
+          <span>Company Info</span>
+          <span>News Letter</span>
+        </div>
+
+        <div>
+        <h5>Social Medias</h5>
+        <span className='mb-2'><i class="fa-brands fa-x-twitter fa-lg"></i></span> <br></br>
+        <span className='mb-2'><i class="fa-brands fa-instagram fa-lg"></i></span> <br></br>
+        <span className='mb-2'><i class="fa-brands fa-facebook fa-lg"></i></span> <br></br>
+        <span><i class="fa-brands fa-telegram fa-lg"></i></span>
+        </div>
+
+      </section>
+
+
+
+
+
+
+    </div>
   )
 }
 
--- src/NavBar.js
+++ src/NavBar.js
@@ -39,14 +39,14 @@ const NavBar = () => {
             
             <div class="container">
               
-            <a class="navbar-brand" href="#" style={{fontWeight:'bolder'}}><img src={logo} style={{width:'60px', borderRadius:'50%',marginRight:'5%'}} />QQQQQQQQQQQQQQQ</a>
+            <a class="navbar-brand" href="#" style={{fontWeight:'bolder'}}><img src={logo} style={{width:'60px', borderRadius:'50%',marginRight:'5%'}} />QQQQ</a>
 
               <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
               </button>
               <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
                 <div class="offcanvas-header">
-                  <h5 class="offcanvas-title" id="offcanvasNavbarLabel">QQQQQQQQQQQQQQQ</h5>
+                  <h5 class="offcanvas-title" id="offcanvasNavbarLabel">QQQQ</h5>
                   <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
                 </div>
                 <div class="offcanvas-body">
